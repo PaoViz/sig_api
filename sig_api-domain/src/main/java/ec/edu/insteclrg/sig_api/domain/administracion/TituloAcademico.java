3f5eb2ab4a1405c2d8e34e7dffe3bd0207ca56e6
dome baquerizo <dome.baquerizo@insteclrg.edu.ec>
2023-06-29 15:20:08 -0500

    domain

--- sig_api-domain/src/main/java/ec/edu/insteclrg/sig_api/domain/administracion/TituloAcademico.java
+++ sig_api-domain/src/main/java/ec/edu/insteclrg/sig_api/domain/administracion/TituloAcademico.java
@@ -1,10 +1,15 @@
 package ec.edu.insteclrg.sig_api.domain.administracion;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
 public class TituloAcademico {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
