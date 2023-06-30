bf0e0661a6de2d56feb29c21094f5972c3f57242
Jessy Yunga <jessyyungaxd@gmail.com>
2023-06-29 20:18:25 -0500

    class creada DTO TipoCertificadoLaboral

---
+++ sig_api-dto/src/main/java/ec/edu/insteclrg/sig_api/dto/administracion/TipoCertificadoLaboralDTO.java
@@ -0,0 +1,18 @@
+package ec.edu.insteclrg.sig_api.dto.administracion;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+public class TipoCertificadoLaboralDTO {
+	
+	private Long id;
+
+	private String descripcion;
+
+}
