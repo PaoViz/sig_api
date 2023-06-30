5d70d58643bbc0f40494b906ff4e9f1627fce1ed
Jessy Yunga <jessyyungaxd@gmail.com>
2023-06-29 21:13:17 -0500

    class Persistence actualizada TipoCertificadoLaboral

--- sig_api-persistence/src/main/java/ec/edu/insteclrg/sig_api/persistence/administracion/TipoCertificadoLaboralPersistence.java
+++ sig_api-persistence/src/main/java/ec/edu/insteclrg/sig_api/persistence/administracion/TipoCertificadoLaboralPersistence.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import ec.edu.insteclrg.sig_api.domain.administracion.Pais;
 import ec.edu.insteclrg.sig_api.domain.administracion.TipoCertificadoLaboral;
 
 public interface TipoCertificadoLaboralPersistence extends JpaRepository<TipoCertificadoLaboral, Long>{
