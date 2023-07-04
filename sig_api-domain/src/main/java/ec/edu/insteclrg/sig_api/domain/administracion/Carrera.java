fb2ee585b0112f18f09f9e25e001f35b81e93dbe
Romulo Coronel <romulo.coronel@insteclrg.edu.ec>
2023-07-03 21:26:46 -0500

    Create class Domain, DTO Carrera

---
+++ sig_api-domain/src/main/java/ec/edu/insteclrg/sig_api/domain/administracion/Carrera.java
@@ -0,0 +1,28 @@
+package ec.edu.insteclrg.sig_api.domain.administracion;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "carrera")
+public class Carrera {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String codigo;
+	
+	@Column(nullable = false, unique = true)
+	private String descripcion;
+}
---
+++ sig_api-dto/src/main/java/ec/edu/insteclrg/sig_api/dto/administracion/CarreraDTO.java
@@ -0,0 +1,20 @@
+package ec.edu.insteclrg.sig_api.dto.administracion;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+public class CarreraDTO {
+	
+	private Long id;
+	
+	private String codigo;
+	
+	private String descripcion;
+
+}
