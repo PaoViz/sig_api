3b64d4589c87562afbc4278523c2ea764c3a7b19
nube Tenezaca <cecilia.tenezaca2001@gmail.com>
2023-07-05 20:30:03 -0500

    CLASE ACTUALIZADA

--- sig_api-service/src/main/java/ec/edu/insteclrg/sig_api/service/administracion/GestionCertificadoService.java
+++ sig_api-service/src/main/java/ec/edu/insteclrg/sig_api/service/administracion/GestionCertificadoService.java
@@ -2,11 +2,14 @@ package ec.edu.insteclrg.sig_api.service.administracion;
 
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
 
 import ec.edu.insteclrg.sig_api.domain.administracion.GestionCertificado;
 import ec.edu.insteclrg.sig_api.dto.administracion.GestionCertificadoDTO;
 import ec.edu.insteclrg.sig_api.service.GenericCrudServiceImpl;
 
+
+@Service
 public class GestionCertificadoService extends GenericCrudServiceImpl<GestionCertificado, GestionCertificadoDTO>{
 
 	@Override
