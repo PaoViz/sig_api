61c9ecbb9b10f377e4282a3533b0ad77b2ca829e
Romulo Coronel <romulo.coronel@insteclrg.edu.ec>
2023-07-11 15:27:31 -0500

    UPDATE ANDMINISTRATIVO CONTROLLER

--- sig_api-app/src/main/java/ec/edu/insteclrg/sig_api/api/v1/administracion/AdministrativoController.java
+++ sig_api-app/src/main/java/ec/edu/insteclrg/sig_api/api/v1/administracion/AdministrativoController.java
@@ -1,13 +1,12 @@
 package ec.edu.insteclrg.sig_api.api.v1.administracion;
 
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ec.edu.insteclrg.dto.ApiResponseDTO;
 import ec.edu.insteclrg.sig_api.common.Constants;
-import ec.edu.insteclrg.sig_api.domain.administracion.Administrativo;
 import ec.edu.insteclrg.sig_api.dto.administracion.AdministrativoDTO;
 import ec.edu.insteclrg.sig_api.service.administracion.AdministrativoService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -41,6 +39,8 @@ public class AdministrativoController {
 			return (new ResponseEntity<Object>(response, HttpStatus.OK));} else {
 			return new ResponseEntity<>(new ApiResponseDTO<>(false, null), HttpStatus.NOT_FOUND);
 		}
+		
+		
 	}
 
 	@Operation(summary = "Guarda un nuevo Administrativo")
