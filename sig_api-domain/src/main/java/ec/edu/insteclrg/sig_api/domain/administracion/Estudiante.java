9dd3b1d9dc009bedfbd1160b2597614e5cf18e1a
Jessy Yunga <jessyyungaxd@gmail.com>
2023-07-13 14:56:18 -0500

    UPDATE Class Estudiante

--- sig_api-domain/src/main/java/ec/edu/insteclrg/sig_api/domain/administracion/Estudiante.java
+++ sig_api-domain/src/main/java/ec/edu/insteclrg/sig_api/domain/administracion/Estudiante.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -24,8 +25,7 @@ public class Estudiante {
 	@Column(updatable = false, nullable = false)
 	private Long id;
 
-	@OneToOne(cascade = CascadeType.ALL, optional = false)
+	@ManyToOne(cascade = CascadeType.ALL, optional = false)
 	@JoinColumn(name = "persona_id")
 	private Persona persona;
-
 }
