1d9487a3e8cc2e1db13fc4794117087f532ec7e6
nube Tenezaca <cecilia.tenezaca2001@gmail.com>
2023-07-13 19:06:25 -0500

    SERVICES ACTUALIZADOS

--- sig_api-service/src/main/java/ec/edu/insteclrg/sig_api/service/administracion/GestionCertificadoService.java
+++ sig_api-service/src/main/java/ec/edu/insteclrg/sig_api/service/administracion/GestionCertificadoService.java
@@ -1,33 +1,36 @@
 package ec.edu.insteclrg.sig_api.service.administracion;
 
 import java.util.Optional;
-
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import ec.edu.insteclrg.sig_api.domain.administracion.GestionCertificado;
 import ec.edu.insteclrg.sig_api.dto.administracion.GestionCertificadoDTO;
+import ec.edu.insteclrg.sig_api.persistence.administracion.GestionCertificadoPersistence;
 import ec.edu.insteclrg.sig_api.service.GenericCrudServiceImpl;
 
 
 @Service
 public class GestionCertificadoService extends GenericCrudServiceImpl<GestionCertificado, GestionCertificadoDTO>{
 
+	@Autowired
+	private GestionCertificadoPersistence repository;
+
+	private ModelMapper modelMapper = new ModelMapper();
+	
 	@Override
 	public Optional<GestionCertificado> find(GestionCertificadoDTO dto) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		return repository.findById(dto.getId());
 	}
 
 	@Override
 	public GestionCertificado mapToDomain(GestionCertificadoDTO dto) {
-		// TODO Auto-generated method stub
-		return null;
+		return modelMapper.map(dto, GestionCertificado.class);
 	}
 
 	@Override
 	public GestionCertificadoDTO mapToDto(GestionCertificado domain) {
-		// TODO Auto-generated method stub
-		return null;
+		return modelMapper.map(domain, GestionCertificadoDTO.class);
 	}
 
 }
--- sig_api-service/src/main/java/ec/edu/insteclrg/sig_api/service/administracion/GrupoOcupacionalService.java
+++ sig_api-service/src/main/java/ec/edu/insteclrg/sig_api/service/administracion/GrupoOcupacionalService.java
@@ -3,29 +3,35 @@ package ec.edu.insteclrg.sig_api.service.administracion;
 
 
 import java.util.Optional;
-
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ec.edu.insteclrg.sig_api.domain.administracion.GrupoOcupacional;
 import ec.edu.insteclrg.sig_api.dto.administracion.GrupoOcupacionalDTO;
+import ec.edu.insteclrg.sig_api.persistence.administracion.GrupoOcupacionalPersistence;
 import ec.edu.insteclrg.sig_api.service.GenericCrudServiceImpl;
 
+@Service
 public class GrupoOcupacionalService  extends GenericCrudServiceImpl<GrupoOcupacional, GrupoOcupacionalDTO> {
 
+	@Autowired
+	private GrupoOcupacionalPersistence repository;
+
+	private ModelMapper modelMapper = new ModelMapper();
+	
 	@Override
 	public Optional<GrupoOcupacional> find(GrupoOcupacionalDTO dto) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		return repository.findById(dto.getId());
 	}
 
 	@Override
 	public GrupoOcupacional mapToDomain(GrupoOcupacionalDTO dto) {
-		// TODO Auto-generated method stub
-		return null;
+		return modelMapper.map(dto, GrupoOcupacional.class);
 	}
 
 	@Override
 	public GrupoOcupacionalDTO mapToDto(GrupoOcupacional domain) {
-		// TODO Auto-generated method stub
-		return null;
+		return modelMapper.map(domain, GrupoOcupacionalDTO.class);
 	}
 	
 
