da16f35490151cf37cbae7c0358c0943335c0ed3
nube Tenezaca <cecilia.tenezaca2001@gmail.com>
2023-07-13 19:12:23 -0500

    Clase service pisEtapa actualizada

--- sig_api-service/src/main/java/ec/edu/insteclrg/sig_api/service/administracion/PisEtapaService.java
+++ sig_api-service/src/main/java/ec/edu/insteclrg/sig_api/service/administracion/PisEtapaService.java
@@ -2,27 +2,35 @@ package ec.edu.insteclrg.sig_api.service.administracion;
 
 
 import java.util.Optional;
-
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ec.edu.insteclrg.sig_api.domain.administracion.PisEtapa;
 import ec.edu.insteclrg.sig_api.dto.administracion.PisEtapaDTO;
+import ec.edu.insteclrg.sig_api.persistence.administracion.PisEtapaPersistence;
 import ec.edu.insteclrg.sig_api.service.GenericCrudServiceImpl;
 
+@Service
 public class PisEtapaService extends GenericCrudServiceImpl<PisEtapa, PisEtapaDTO> {
 
+	@Autowired
+	private PisEtapaPersistence repository;
+
+	private ModelMapper modelMapper = new ModelMapper();
+	
     @Override
     public Optional<PisEtapa> find(PisEtapaDTO dto) {
-        return Optional.empty();
+    	return repository.findById(dto.getId());
     }
 
     @Override
     public PisEtapa mapToDomain(PisEtapaDTO dto) {
-        return null;
+    	return modelMapper.map(dto, PisEtapa.class);
     }
 
 	@Override
 	public PisEtapaDTO mapToDto(PisEtapa domain) {
-		// TODO Auto-generated method stub
-		return null;
+		return modelMapper.map(domain, PisEtapaDTO.class);
 	}
 
 }
