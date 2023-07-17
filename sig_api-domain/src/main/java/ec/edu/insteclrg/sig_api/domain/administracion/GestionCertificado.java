2897e59e19270642436383b104da8ee2e3ea8e19
nube Tenezaca <cecilia.tenezaca2001@gmail.com>
2023-07-17 14:33:51 -0500

    clase actualizada gestion certificado  domain

--- sig_api-domain/src/main/java/ec/edu/insteclrg/sig_api/domain/administracion/GestionCertificado.java
+++ sig_api-domain/src/main/java/ec/edu/insteclrg/sig_api/domain/administracion/GestionCertificado.java
@@ -1,6 +1,7 @@
 package ec.edu.insteclrg.sig_api.domain.administracion;
 
-import lombok.ToString;
+import java.sql.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,7 +10,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,7 +26,7 @@ public class GestionCertificado {
     private Long id;
 
     @Column()
-    private Data fechaCreacion;
+    private Date fechaCreacion;
 
     @Column()
     private String titulo;
