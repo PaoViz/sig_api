3be71c776d67693ef443987b9d35d294a84714b8
nube Tenezaca <cecilia.tenezaca2001@gmail.com>
2023-07-17 14:37:05 -0500

    clase actualizada gestionCertificadoDTO

--- sig_api-dto/src/main/java/ec/edu/insteclrg/sig_api/dto/administracion/GestionCertificadoDTO.java
+++ sig_api-dto/src/main/java/ec/edu/insteclrg/sig_api/dto/administracion/GestionCertificadoDTO.java
@@ -1,9 +1,13 @@
 package ec.edu.insteclrg.sig_api.dto.administracion;
 
+
+
+
+
+import java.sql.Date;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import java.util.Date;
 
 @Getter
 @Setter
