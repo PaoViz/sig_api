acbea75ab268e40206c7c5d2ed1469fbef4a94f9
Jessy Yunga <jessyyungaxd@gmail.com>
2023-08-03 21:24:30 -0500

    ADD CLASS Investigación Etapa domain and DTO

---
+++ sig_api-dto/src/main/java/ec/edu/insteclrg/sig_api/dto/administracion/InvestigacionEtapaDTO.java
@@ -0,0 +1,17 @@
+package ec.edu.insteclrg.sig_api.dto.administracion;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class InvestigacionEtapaDTO {
+
+private Long id;
+	
+	private String descripcion;
+	
+	private ProyectoDTO proyectoacademico;
+}
