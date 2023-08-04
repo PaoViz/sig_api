1da8661d42578e699ff9d81acec61048c3f4a0f9
Angie Vizhñay <paolaviz122333@gmail.com>
2023-08-03 21:03:59 -0500

    Class ProyectoVinculacionTipo update

--- sig_api-common/src/main/java/ec/edu/insteclrg/sig_api/common/enums/ProyectoVinculacionTipo.java
+++ sig_api-common/src/main/java/ec/edu/insteclrg/sig_api/common/enums/ProyectoVinculacionTipo.java
@@ -2,5 +2,21 @@ package ec.edu.insteclrg.sig_api.common.enums;
 
 public enum ProyectoVinculacionTipo {
 	
-	VINCULACION, ACTIVIDADDEVINCULACIÓN
+	VINCULACION(1,"Vinculación"), 
+	ACTIVIDADDEVINCULACIÓN(2,"Actividad de Vinculación");
+
+	private int id;
+	private String description;
+
+	public int getId() {
+		return id;
+	}
+	public String getDescription() {
+		return description;
+	}
+
+	private ProyectoVinculacionTipo(int id, String descripcion) {
+		this.id=id;
+		this.description = descripcion;
+	}
 }
