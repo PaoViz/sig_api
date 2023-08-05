fed729f618ba6adfe20a0f1d6ae98af072fd3f21
nube Tenezaca <cecilia.tenezaca2001@gmail.com>
2023-08-05 17:04:31 -0500

    CLASE CREADA  INVESTIGACIONDOCENTE

---
+++ sig_api-dto/src/main/java/ec/edu/insteclrg/sig_api/dto/administracion/InvestigacionDocenteDTO.java
@@ -0,0 +1,16 @@
+package ec.edu.insteclrg.sig_api.dto.administracion;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class InvestigacionDocenteDTO {
+
+
+		private Long id;
+		private Long idProyectoAcademico;
+		private Long idDocente;
+}
