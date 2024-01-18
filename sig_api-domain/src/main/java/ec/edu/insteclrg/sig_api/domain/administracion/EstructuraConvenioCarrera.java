722c773241054c90205c4478b20009254c5519e4
Angie <Angie@192.168.0.106>
2024-01-18 16:42:09 -0500

    Created EstructuraConvenioCarrera Class

---
+++ sig_api-domain/src/main/java/ec/edu/insteclrg/sig_api/domain/administracion/EstructuraConvenioCarrera.java
@@ -0,0 +1,33 @@
+package ec.edu.insteclrg.sig_api.domain.administracion;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "estructura_convenio_carrera")
+public class EstructuraConvenioCarrera {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private Long id;
+	
+	@ManyToOne(cascade = CascadeType.ALL, optional = false)
+	@JoinColumn(name = "gestionDocumental_id")
+	private GestionDocumentalConvenio gestionDocumentalConvenio;
+	
+	@ManyToOne(cascade = CascadeType.ALL, optional = false)
+	@JoinColumn(name = "carrera_id")
+	private Carrera carrera;
+}
