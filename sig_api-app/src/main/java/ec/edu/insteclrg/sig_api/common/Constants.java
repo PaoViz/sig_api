a41d723b12745d5df908314565b1f6f04ea07ed2
Angie <Angie@DESKTOP-PAMDRTM>
2024-01-23 14:20:42 -0500

    Update Constants Class

--- sig_api-app/src/main/java/ec/edu/insteclrg/sig_api/common/Constants.java
+++ sig_api-app/src/main/java/ec/edu/insteclrg/sig_api/common/Constants.java
@@ -51,6 +51,7 @@ public class Constants {
 	public static final String URI_API_CONTRATO_LABORLA = URI_API_V1 + "contrato_laboral";
 	public static final String URI_API_GESTION_CERTIFICADO = URI_API_V1 + "gestion_certificado";
 	public static final String URI_API_TIPO_ADMINISTRATIVO = URI_API_V1 + "tipo_administrativo";
+	public static final String URI_API_ESTRUCTURA_CONVENIO_CARRERA = URI_API_V1 + "estructura_convenio_carrera";
 
 
 
