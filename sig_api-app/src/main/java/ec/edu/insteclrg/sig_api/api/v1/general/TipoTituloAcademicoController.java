2e38ccc91dbc7887d8d4364de0152a9fe47c4b03
Angie <Angie@192.168.0.106>
2024-01-24 00:59:52 -0500

    Controller Package Update

--- sig_api-app/src/main/java/ec/edu/insteclrg/sig_api/api/v1/general/TipoTituloAcademicoController.java
+++ sig_api-app/src/main/java/ec/edu/insteclrg/sig_api/api/v1/general/TipoTituloAcademicoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +23,7 @@ import ec.edu.insteclrg.sig_api.service.general.TipoTituloAcademicoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-
+@CrossOrigin (origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = { Constants.URI_API_TIPO_TITULO_ACADEMICO })
 @Tag(name = "Tipo Titulo Académico", description = "Gestiona titulo academico (ej.tercer nivel, cuarto nivel)")
