9204ebe24448b26ed2498e10453307c0052ac796
Angie <Angie@192.168.0.106>
2024-02-03 19:57:49 -0500

    Delete TTHH Persistence class

--- sig_api-persistence/src/main/java/ec/edu/insteclrg/sig_api/persistence/talentohumano/AdministrativoPersistence.java
+++
@@ -1,9 +0,0 @@
-package ec.edu.insteclrg.sig_api.persistence.talentohumano;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ec.edu.insteclrg.sig_api.domain.talentohumano.Administrativo;
-
-public interface AdministrativoPersistence extends JpaRepository<Administrativo, Long>{
-
-}
--- sig_api-persistence/src/main/java/ec/edu/insteclrg/sig_api/persistence/talentohumano/CertificadoLaboralPersistence.java
+++
@@ -1,9 +0,0 @@
-package ec.edu.insteclrg.sig_api.persistence.talentohumano;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ec.edu.insteclrg.sig_api.domain.talentohumano.CertificadoLaboral;
-
-public interface CertificadoLaboralPersistence extends JpaRepository<CertificadoLaboral, Long> {
-
-}
