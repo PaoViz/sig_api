a516d8c53f3f6232dc3ec7770358ff6175cecf85
Angie <Angie@192.168.0.106>
2024-02-03 20:36:28 -0500

    Delete Investigacion Persistence Class

--- sig_api-persistence/src/main/java/ec/edu/insteclrg/sig_api/persistence/investigacion/InvestigacionDocentePersistence.java
+++
@@ -1,9 +0,0 @@
-package ec.edu.insteclrg.sig_api.persistence.investigacion;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ec.edu.insteclrg.sig_api.domain.investigacion.InvestigacionDocente;
-
-public interface InvestigacionDocentePersistence extends JpaRepository<InvestigacionDocente, Long>{
-
-}
