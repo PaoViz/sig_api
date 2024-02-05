532bff435f1b4c346d4292e28149e75ec655875f
Angie <Angie@DESKTOP-PAMDRTM>
2024-02-04 19:27:11 -0500

    Update PersonaDTO class

--- sig_api-dto/src/main/java/ec/edu/insteclrg/sig_api/dto/general/PersonaDTO.java
+++ sig_api-dto/src/main/java/ec/edu/insteclrg/sig_api/dto/general/PersonaDTO.java
@@ -2,7 +2,6 @@ package ec.edu.insteclrg.sig_api.dto.general;
 
 import java.sql.Date;
 
-import ec.edu.insteclrg.sig_api.dto.investigacion.PaisDTO;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
