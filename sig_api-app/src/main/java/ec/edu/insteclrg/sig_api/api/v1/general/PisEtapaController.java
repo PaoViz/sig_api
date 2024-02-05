58ed8a70ae273bbe2c7b33d0b6674479122f3aa5
Angie <Angie@DESKTOP-PAMDRTM>
2024-02-04 19:47:55 -0500

    Created PisEtapaController class

---
+++ sig_api-app/src/main/java/ec/edu/insteclrg/sig_api/api/v1/general/PisEtapaController.java
@@ -0,0 +1,71 @@
+package ec.edu.insteclrg.sig_api.api.v1.general;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ec.edu.insteclrg.sig_api.common.Constants;
+import ec.edu.insteclrg.sig_api.dto.administracion.ApiResponseDTO;
+import ec.edu.insteclrg.sig_api.dto.general.PaisDTO;
+import ec.edu.insteclrg.sig_api.dto.general.PisEtapaDTO;
+import ec.edu.insteclrg.sig_api.service.general.PisEtapaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+
+@CrossOrigin (origins = "http://localhost:4200")
+@RestController
+@RequestMapping(value = { Constants.URI_API_PIS_ETAPA })
+@Tag(name = "PisEtapa", description = "Gestiona las etapas del documento pis(ej. estructura de temas, Avance 1")
+public class PisEtapaController {
+
+	@Autowired
+	PisEtapaService service;
+	
+	@Operation(summary = "Lista todos las etapas")
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> listar() {
+		List<PisEtapaDTO> list = service.findAll(new PisEtapaDTO());
+		if (!list.isEmpty()) {
+			ApiResponseDTO<List<PisEtapaDTO>> response = new ApiResponseDTO<>(true, list);
+			return (new ResponseEntity<Object>(response, HttpStatus.OK));
+		} else {
+			return new ResponseEntity<>(new ApiResponseDTO<>(false, null), HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@Operation(summary = "Guarda una nueva etapa")
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> guardar(@RequestBody PisEtapaDTO PisEtapaDTO) {
+		PisEtapaDTO PisEtapaDTOResult = service.save(PisEtapaDTO);
+		return new ResponseEntity<>(new ApiResponseDTO<>(true, PisEtapaDTOResult), HttpStatus.CREATED);
+	}
+
+	@Operation(summary = "Actualiza una etapa")
+	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> actualizar(@RequestBody PisEtapaDTO PisEtapaDTO) {
+		PisEtapaDTO resultDTO = service.update(PisEtapaDTO);
+		return new ResponseEntity<>(new ApiResponseDTO<>(true, resultDTO), HttpStatus.CREATED);
+	}
+
+	@Operation(summary = "Recupera por id una etapa")
+	@GetMapping(value = "{id}/archivo/id", produces = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<Object> buscarPorId(@Valid @PathVariable("id") long id) {
+		PisEtapaDTO dto = new PisEtapaDTO();
+		dto.setId(id);
+		return new ResponseEntity<>(new ApiResponseDTO<>(true, service.find(dto)), HttpStatus.OK);
+	}
+
+}
+
